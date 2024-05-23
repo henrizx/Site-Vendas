6ec38a6ff8c1b915e503391669d88397f0482ae4
henrizx <marcelohenrizx@gmail.com>
2024-05-23 15:11:37 -0300

    incluindo duas lojas

--- JavaScripts/geral.js
+++ JavaScripts/geral.js
@@ -6,3 +6,22 @@ visitBtn.addEventListener('click', function() {
     // Redireciona para a página desejada
     window.location.href = 'marcio.html';
 });
+
+
+// Seleciona o botão pelo ID
+const visitBtnG = document.getElementById('btn-gio');
+
+// Adiciona um ouvinte de evento de clique ao botão
+visitBtnG.addEventListener('click', function() {
+    // Redireciona para a página desejada
+    window.location.href = 'gio.html';
+});
+
+const visitBtnM = document.getElementById('btn');
+
+// Adiciona um ouvinte de evento de clique ao botão
+visitBtnM.addEventListener('click', function() {
+    // Redireciona para a página desejada
+    window.location.href = 'tiao.html';
+});
+
